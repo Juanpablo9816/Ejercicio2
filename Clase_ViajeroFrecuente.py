b23156654d20a7cdf24a7778c9ec9fe748999666
Juanpablo9816 <73374079+Juanpablo9816@users.noreply.github.com>
2020-10-24 00:31:12 -0300

    Create Clase_ViajeroFrecuente.py

---
+++ Clase_ViajeroFrecuente.py
@@ -0,0 +1,23 @@
+class ViajeroFrecuente():
+    __NViajero = 0
+    __DNI = 0
+    __Nombre = ""
+    __Apellido = ""
+    __Millas = 0.0
+
+    def __init__(self,NV,DNI,Nom,Apell,Mill):
+        self.__NViajero = NV
+        self.__DNI = DNI
+        self.__Nombre = Nom
+        self.__Apellido = Apell
+        self.__Millas = Mill
+
+    def AcumularMillas(self,Mill):
+        self.__Millas =self.__Millas+Mill
+    def CanjearMillas(self,Mill):
+        pass
+    def getnum(self):
+        return self.__NViajero
+    def cantidadTotaldeMillas(self):
+        return self.__Millas
+    
